80ec7b2612f46bc77e9c968ae0cbdeb29d304cc9
ojasdeshpnde <ojas.a.deshpande@gmail.com>
2022-01-07 02:51:21 -0500

    Update RandomTerrainGeneration.py

--- RandomTerrainGeneration.py
+++ RandomTerrainGeneration.py
@@ -8,31 +8,49 @@ downCount = 0
 
 def exploreRight():
     global rightCount
-    pic2 = [noise([1+rightCount/xpix, i/ypix]) for i in range(ypix)]
+    pic2 = [noise([1+rightCount/xpix, (i + downCount)/ypix]) for i in range(ypix)]
     rightCount = rightCount +1
-    del pic[0]
     pic.append(pic2)
-    paint()
+    del pic[0]
+    #paint()
 
 def exploreDown():
     global downCount
-    pic2 = [noise([i/xpix, 1+downCount/ypix]) for i in range(ypix)]
+    pic2 = [noise([(i + rightCount)/xpix, 1+downCount/ypix]) for i in range(ypix)]
     downCount = downCount +1
     for i in range(xpix):
         pic[i].append(pic2[i])
         del pic[i][0]
-    paint()
+    #paint()
 
+def exploreUp():
+    global downCount
+    pic2 = [noise([(i + rightCount) / xpix, downCount / ypix]) for i in range(ypix)]
+    downCount = downCount -1
+    for i in range(xpix):
+        pic[i].insert(0,pic2[i])
+        del pic[i][xpix]
+    #paint()
+
+def exploreLeft():
+    global rightCount
+    pic2 = [noise([rightCount / xpix, (i + downCount) / ypix]) for i in range(ypix)]
+    rightCount = rightCount - 1
+    pic.insert(0,pic2)
+    del pic[ypix]
+    #paint()
 
 def paint():
     for i in range(0, xpix):
         for j in range(0, ypix):
             if (pic[i][j] < waterElement):
-                pygame.draw.rect(screen, BLUE, (i * pixelSize, j * pixelSize, pixelSize, pixelSize))
+                pygame.draw.rect(screen, BLUE, ((i-0) * pixelSize, (j-0) * pixelSize, pixelSize, pixelSize))
             elif (pic[i][j] < sandElement):
-                pygame.draw.rect(screen, SAND, (i * pixelSize, j * pixelSize, pixelSize, pixelSize))
+                pygame.draw.rect(screen, SAND, ((i-0) * pixelSize, (j-0) * pixelSize, pixelSize, pixelSize))
+            elif (pic[i][j] < greenElement):
+                pygame.draw.rect(screen, GREEN, ((i-0) * pixelSize, (j-0) * pixelSize, pixelSize, pixelSize))
             else:
-                pygame.draw.rect(screen, GREEN, (i * pixelSize, j * pixelSize, pixelSize, pixelSize))
+                pygame.draw.rect(screen, STONE, ((i-0) * pixelSize, (j-0) * pixelSize, pixelSize, pixelSize))
 
     pygame.display.flip()
 
@@ -40,6 +58,7 @@ def paint():
 BLUE = (0,0,255)
 SAND = (194,178,128)
 GREEN = (0,128,0)
+STONE = (145, 142, 133)
 
 pygame.init()
 screen = pygame.display.set_mode([1000,1000])
@@ -54,7 +73,8 @@ picList.sort()
 
 
 waterElement = picList[int(xpix * ypix * 0.6)]
-sandElement = picList[int(xpix * ypix * 0.8)]
+sandElement = picList[int(xpix * ypix * 0.7)]
+greenElement = picList[int(xpix * ypix * 0.9)]
 
 paint()
 run = True
@@ -69,3 +89,8 @@ while run:
         exploreRight()
     if keys[pygame.K_DOWN]:
         exploreDown()
+    if keys[pygame.K_UP]:
+        exploreUp()
+    if keys[pygame.K_LEFT]:
+        exploreLeft()
+    paint()
